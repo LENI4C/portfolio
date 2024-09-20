377127952e0beb16768345e006ba65d1b5571640
LENI4C <timibraperetei74@gmail.com>
2024-09-20 15:26:50 +0100

    bug adjustment

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+
+export const App = () => {
+  return (
+    <div>Sleek Portfolio</div>
+  )
+}
+
