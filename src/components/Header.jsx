7190c1b2aae151a1c44d342f82b7c073b1c4a41a
LENI4C <timibraperetei74@gmail.com>
2024-09-23 15:37:33 +0100

    good start

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,14 @@ import { MdClose } from "react-icons/md";
 import { MdOutlineDarkMode } from "react-icons/md";
 import { MdLightMode } from "react-icons/md";
 
-
-
 const Header = () => {
     return (
-        <header className="fixed top-0 bg-white w-full backdrop-blur bg-opacity-60 p-4 flex">
+        <header className="fixed top-0 bg-white w-full backdrop-blur bg-opacity-60 p-4 flex justify-between">
             <h1 className="text-lg font-poppins">Leni4c</h1>
             <GiHamburgerMenu />
             <MdClose />
             <MdOutlineDarkMode />
             <MdLightMode />
-
         </header>
     );
 };
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,12 +2,14 @@ import leni4c from "../assets/images/leni4c.jpg";
 
 const Main = () => {
     return (
-        <main className="mt-20">
+        <main className="mt-20 p-8">
             <img
                 src={leni4c}
                 alt="leni4c"
                 className="w-40 h-40 rounded-full object-cover object-bottom"
             />
+            <h1 className="m-4 text-xl font-poppins font-semibold">Hi 👋, I'm Leni4c - Web Developer 💻</h1>
+            <p> I’m passionate about turning ideas into digital realities! Whether it's building something from scratch or enhancing existing web experiences, I thrive on crafting websites and apps that not only look great but perform even better. </p>
         </main>
     );
 };
